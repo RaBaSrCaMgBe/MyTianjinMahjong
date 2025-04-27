b19514fd77d9250320dcb427569b74b5e752aea6
神仙舞见gagako <wujunaomc@gmail.com>
2025-04-27 18:44:51 +0800

    update:新增多房间功能
    Signed-off-by: 神仙舞见gagako <wujunaomc@gmail.com>

--- room.py
+++ room.py
@@ -1,5 +1,6 @@
 import asyncio
 import json
+from game import Game
 
 
 class Room:
@@ -7,10 +8,12 @@ class Room:
         self.player_list = ['' for i in range(4)]
         self.client_list = [None for i in range(4)]
         self.player_prepare = [0 for i in range(4)]
+        self.player_cnt = 0
         self.prepared_player_cnt = 0
         self.game_active = False
         self.room_lock = asyncio.Lock()
         self.room_barrier = asyncio.Barrier(4)
+        self.game = Game()
 
     async def broadcast_room_status(self):
         response = {'type': 'room_update', 'player_list': self.player_list, 'player_prepare': self.player_prepare}
